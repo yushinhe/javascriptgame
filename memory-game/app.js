178708fbdb19f9b4126fef415a6d1c41e2a42e5d
yushinhe <enter>
2020-08-26 18:25:37 +0800

    寫board

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,3 +1,51 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+        {
+            name: 'baseballman',
+            img: 'ima/baseballman.png',
+        },
+        {
+            name: 'baseballwoman',
+            img: 'images/baseballwoman.png',
+        },
+        {
+            name: 'hurdleman',
+            img: 'images/hurdleman.png',
+        },
+        {
+            name: 'baseballman',
+            img: 'images/hurdlewoman.png',
+        },
+        {
+            name: 'marathonman',
+            img: 'images/marathonman.png',
+        },
+        {
+            name: 'paralympicrunner',
+            img: 'images/paralympicrunner.png',
+        },
+        {
+            name: 'rikujouhabatobi',
+            img: 'images/rikujouhabatobi.png',
+        },
+        {
+            name: 'soccerman',
+            img: 'images/soccerman.png',
+        },
+        {
+            name: 'soccerwoman',
+            img: 'images/soccerwoman.png',
+        },
+    ]
+    const grid =document.querySelector('.grid')
+    //create your board
+    function createBoard(){
+        for(let i=0;i<cardArray.length;i++){
+            var card =document.createElement('img')
+            card.setAttribute('src','images/blanks.png')
+            card.setAttribute('data-id',i)
+            grid.appendChild(card) 
+        }
+    }
+    //check
 })
