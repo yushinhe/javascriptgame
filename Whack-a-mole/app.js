33d9f65148ddee878f7a9745f1c105f1005604e3
yushinhe <robin124708476@gmail.com>
2020-08-27 17:36:32 +0800

    打地鼠

---
+++ Whack-a-mole/app.js
@@ -0,0 +1,35 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+let result = 0
+let currentTime = timeLeft.textContent
+function randomSquare() {
+    square.forEach(className => {className.classList.remove('mole') })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+    hitPosition = randomPosition.id
+}
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if (id.id === hitPosition) {
+            result = result + 1
+            score.textContent = result
+            hitPosition=null
+        }
+    })
+})
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+}
+moveMole()
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+    if (currentTime === 0) {
+        clearInterval(timerId)
+        alert('Game Over Your Final Score is' + result)
+    }
+}
+let timerId = setInterval(countDown, 1000)
